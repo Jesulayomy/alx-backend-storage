d7077b2d05a7e045df6a4abf10c9eb5856dccf47
Jesulayomy <jesulayomi081@gmail.com>
2023-08-16 22:48:02 +0000

    Advanced

---
+++ 0x02-Redis/web.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" This module showcases expiring web caches """
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redDb = redis.Redis()
+
+
+def dCache(method: Callable) -> Callable:
+    """ Temporarily left the countr """
+
+    @wraps(method)
+    def invoker(url) -> str:
+        """ Wrapper for the get_page and iscariot"""
+
+        redDb.incr(f'count:{url}')
+        result = redDb.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redDb.set(f'count:{url}', 0)
+        redDb.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@dCache
+def get_page(url: str) -> str:
+    """ Return url content so you can see if you qualify"""
+    
+    return requests.get(url).text
